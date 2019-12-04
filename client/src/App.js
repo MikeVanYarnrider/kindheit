aaa3220ae6cf80991950627edefe2c310bb8db93
Pascal Hanke <pascal.hanke@yahoo.de>
2019-12-04 13:36:47 +0100

    added-game-dragndrop

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Puzzle from "./components/Games/Puzzle/Puzzle";
 
 function App() {
   return (
     <div>
+      <Puzzle />
       <h1>Hello!</h1>
     </div>
   );
