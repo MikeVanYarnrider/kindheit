f54c1f3bf3fca2aefd7230d2fd160ec1a167846f
Michael Garnreiter <mgarnreiter@aol.com>
2019-12-05 11:53:17 +0100

    add game slider

---
+++ client/src/components/Slide.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../assets/stylesheet/components/slide.scss";
+
+const Slide = ({ image }) => {
+  const styles = {
+    backgroundImage: `url(${image})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "50% 60%"
+  };
+  return <div className="slide" style={styles}></div>;
+};
+
+export default Slide;
