1f06c5919d3b69a6c943f804e1d5cb9572f23f2f
Iuliia Rozhnova <Juliarakhmetova@live.ru>
2019-12-05 13:09:31 +0100

    Connect games DB

---
+++ bin/data.js
@@ -0,0 +1,25 @@
+const games = [
+  {
+    title: "Fold a train",
+    description: "",
+    totalPlayTime: 0,
+    minAge: 4,
+    type: "handsgames"
+  },
+  {
+    title: "Puzzle",
+    description: "",
+    totalPlayTime: 0,
+    minAge: 4,
+    type: "device"
+  },
+  {
+    title: "Memory Game",
+    description: "",
+    totalPlayTime: 0,
+    minAge: 4,
+    type: "device"
+  }
+];
+
+module.exports = games;
