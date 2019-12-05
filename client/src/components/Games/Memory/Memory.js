f95eee6257b1fe400aea44a668b0ee6581349e1d
Iuliia Rozhnova <Juliarakhmetova@live.ru>
2019-12-05 22:10:44 +0100

    Add logic for memory game

--- client/src/components/Games/Memory/Memory.js
+++ client/src/components/Games/Memory/Memory.js
@@ -1,8 +1,117 @@
 import React, { Component } from "react";
 
-class Memory extends Component {
+import "../../../assets/stylesheet/components/memory.scss";
+
+import {
+  memoryImage1,
+  memoryImage2,
+  memoryImage3,
+  memoryImage4
+} from "../../../images";
+
+const cards = [
+  { name: "teddy", img: memoryImage1 },
+  { name: "elephant", img: memoryImage2 },
+  { name: "lion", img: memoryImage3 },
+  { name: "monkey", img: memoryImage4 }
+];
+
+class Card extends Component {
+  handleClick = card => {
+    this.props.onClick(card);
+  };
+
   render() {
-    return <div>This is a memory game!</div>;
+    return (
+      <div
+        className={`card ${this.props.flipped && "turned"}`}
+        onClick={() => this.handleClick(this.props.card.name)}
+      >
+        <div className="card-back"></div>
+        <div className="card-front">
+          <img src={this.props.card.img} alt={this.props.card.name} />
+        </div>
+      </div>
+    );
+  }
+}
+
+class Memory extends Component {
+  state = {
+    cards: cards,
+    shuffledCards: this.shuffleCards(cards),
+    pickedCards: [],
+    flippedCards: []
+  };
+
+  shuffleCards(arr) {
+    const mixed = Array(2)
+      .fill(arr)
+      .flat();
+
+    let currentIndex = mixed.length,
+      tempValue,
+      randomIndex;
+
+    while (currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      tempValue = mixed[currentIndex];
+      mixed[currentIndex] = mixed[randomIndex];
+      mixed[randomIndex] = tempValue;
+    }
+
+    return mixed;
+  }
+
+  pickCard(card) {
+    let flipped = this.state.shuffledCards.find((el, index) => index === card);
+
+    this.setState(
+      {
+        pickedCards: [...this.state.pickedCards, card],
+        flippedCards: [...this.state.flippedCards, flipped.name]
+      },
+      () => {
+        this.checkPair();
+      }
+    );
+  }
+
+  checkPair = _ => {
+    if (this.state.flippedCards.length === 2) {
+      if (this.state.flippedCards[0] === this.state.flippedCards[1]) {
+        this.setState({
+          flippedCards: []
+        });
+      } else {
+        setTimeout(() => {
+          const arr = this.state.pickedCards.slice(0, -2);
+          this.setState({
+            flippedCards: [],
+            pickedCards: arr
+          });
+        }, 750);
+      }
+    }
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.shuffledCards.map((card, index, arr) => {
+          return (
+            <Card
+              key={index}
+              card={card}
+              flipped={this.state.pickedCards.includes(index)}
+              onClick={() => this.pickCard(index)}
+            />
+          );
+        })}
+      </div>
+    );
   }
 }
 
--- client/src/images.js
+++ client/src/images.js
@@ -9,6 +9,12 @@ import pathImage3 from "./assets/images/games/puzzle/3.png";
 import pathImage4 from "./assets/images/games/puzzle/4.png";
 import pathBGImage from "./assets/images/games/puzzle/bg.png";
 
+// Memory game
+import memoryImage1 from "./assets/images/games/memory/1.png";
+import memoryImage2 from "./assets/images/games/memory/2.png";
+import memoryImage3 from "./assets/images/games/memory/3.png";
+import memoryImage4 from "./assets/images/games/memory/4.png";
+
 export {
   logo,
   lego,
@@ -16,5 +22,9 @@ export {
   pathImage2,
   pathImage3,
   pathImage4,
-  pathBGImage
+  pathBGImage,
+  memoryImage1,
+  memoryImage2,
+  memoryImage3,
+  memoryImage4
 };
