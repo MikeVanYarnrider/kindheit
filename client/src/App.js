d40408e9b50ebdc4da3560159e57aa3f9fd50182
Michael Garnreiter <mgarnreiter@aol.com>
2019-12-05 11:53:43 +0100

    add button component functions to handle routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Puzzle from "./components/Games/Puzzle/Puzzle";
 import Navbar from "./components/Navbar";
-import Button from "./components/Button";
+// import Button from "./components/Button";
 import Login from "./components/Login";
 import GameStart from "./components/Start";
 import GameType from "./components/game/GameType";
 import GameList from "./components/game/GameList";
+import Slider from "./components/Slider";
 
-function App() {
+function App(props) {
+  console.log("app", props);
   return (
     <div>
       <Navbar />
@@ -18,7 +20,13 @@ function App() {
         <Route exact path="/play" component={GameType} />
         <Route exact path="/play/:type" component={GameList} />
         <Route exact path="/play/device/puzzle" component={Puzzle} />
+        <Route
+          exact
+          path="/play/handgames/foldtrain"
+          render={props => <Slider {...props} />}
+        />
       </Switch>
+      <Slider />
     </div>
   );
 }
--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -1,20 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/stylesheet/components/Button.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
   faLongArrowAltLeft,
+  faLongArrowAltRight,
   faPlus,
   faCheck,
   faDice
 } from "@fortawesome/free-solid-svg-icons";
 
 const Button = props => {
-  // console.log(props);
+  console.log(props);
+  const { onClick, href } = props;
+  // console.log(onClick);
 
   const variant = () => {
     if (props.variant.includes("back")) {
       return <FontAwesomeIcon icon={faLongArrowAltLeft} />;
+    }
+    if (props.variant.includes("forward")) {
+      return <FontAwesomeIcon icon={faLongArrowAltRight} />;
     }
     if (props.variant.includes("play")) {
       return <FontAwesomeIcon icon={faPlay} />;
@@ -39,7 +46,19 @@ const Button = props => {
     }
   };
 
-  return <button className={props.variant}>{variant()}</button>;
+  const button = href ? (
+    <Link to={href}>
+      <span role="button" className={props.variant} onClick={onClick}>
+        {variant()}
+      </span>
+    </Link>
+  ) : (
+    <span role="button" className={props.variant} onClick={onClick}>
+      {variant()}
+    </span>
+  );
+
+  return button;
 };
 
 export default Button;
