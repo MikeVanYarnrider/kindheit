8201d3a86a0e09bb3b08f00e28b4c9632127cbf8
Iuliia Rozhnova <Juliarakhmetova@live.ru>
2019-12-06 20:13:52 +0100

    Add instructions modal

---
+++ client/src/components/Games/Puzzle/Instructions.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Instructions = () => {
+  return <div>Puzzle instructions</div>;
+};
+
+export default Instructions;
--- client/src/data/games.js
+++ client/src/data/games.js
@@ -1,25 +1,35 @@
+import React from "react";
+
 import Puzzle from "../components/Games/Puzzle/Puzzle";
+import PuzzleInstructions from "../components/Games/Puzzle/Instructions";
+
 import FoldTrain from "../components/Games/FoldTrain/FoldTrain";
+import FoldTrainInstructions from "../components/Games/FoldTrain/Instructions";
+
 import MemoryGame from "../components/Games/Memory/Memory";
+import MemoryInstructions from "../components/Games/Memory/Instructions";
 
 const games = [
   {
     title: "Fold a train",
     link: "foldtrain",
     type: "handsgames",
-    component: FoldTrain
+    component: FoldTrain,
+    instructions: <FoldTrainInstructions />
   },
   {
     title: "Puzzle",
     link: "puzzle",
     type: "device",
-    component: Puzzle
+    component: Puzzle,
+    instructions: <PuzzleInstructions />
   },
   {
     title: "Memory Game",
     link: "memory",
     type: "device",
-    component: MemoryGame
+    component: MemoryGame,
+    instructions: <MemoryInstructions />
   }
 ];
 
