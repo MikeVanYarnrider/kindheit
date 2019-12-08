12ff343e07c9b128a6aa250d4c321150cd423100
Michael Garnreiter <mgarnreiter@aol.com>
2019-12-08 19:31:24 +0100

    code cleanup

--- client/src/components/ParentsBackend.js
+++ client/src/components/ParentsBackend.js
@@ -19,13 +19,9 @@ class ParentsBackend extends React.Component {
         }
       );
     });
-    // console.log(this.props.parentUser.username);
   }
 
   render() {
-    // console.log(this.state.children);
-    // console.log(this.state.children.username);
-    // const children = this.state.children;
     const children = Object.values(this.state.children).map((child, index) => {
       let timeSum = 0;
       const time = child.sessionTimes.map(times => {
@@ -39,12 +35,11 @@ class ParentsBackend extends React.Component {
           </div>
         );
       });
-      console.log(timeSum);
-      console.log(time);
+
       const { _id, username, profileImgUrl } = child;
       return (
         <div key={_id}>
-          <img src={profileImgUrl} width="100px" alt="profile image" />
+          <img src={profileImgUrl} width="100px" alt="profile" />
           <div>
             <p>
               <strong>Name:</strong> {username}
@@ -59,16 +54,16 @@ class ParentsBackend extends React.Component {
       );
     });
 
-    // console.log(children);
-
+    console.log(this.state.children);
     return (
       <div>
         <h1>Hello {this.props.parentUser.username}</h1>
-        <div>{children}</div>
-        {this.state.children.length === 0 && (
+        {this.state.children.length === 0 || !this.state.children ? (
           <h2>
             No children available. Create <Link to="/childsignup">here</Link>
           </h2>
+        ) : (
+          <div>{children}</div>
         )}
       </div>
     );
@@ -76,14 +71,3 @@ class ParentsBackend extends React.Component {
 }
 
 export default ParentsBackend;
-
-// parent: [ 5debb1e9c6f1e7751f24aa8b ],
-//   gameStatus: [],
-//   sessionTimes: [ 2.446, 8.801 ],
-//   _id: 5debb1fdc6f1e7751f24aa8c,
-//   username: 'kid1',
-//   password: '$2b$10$IF5c875qziPmYbf3aJ7Un.W5NjHyou.FeDiHjD.kF6XKq0l/iVJse',
-//   birthDate: 0001-01-01T00:00:00.000Z,
-//   profileImgUrl: '/static/media/dragon.4a6b00d5.png',
-//   type: 'child',
-//   __v: 0
