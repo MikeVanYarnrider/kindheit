ac11402a5769a413cec8953be77ff99ee764b1ef
Michael Garnreiter <mgarnreiter@aol.com>
2019-12-08 14:10:19 +0100

    add kids profiles to parent backend with calculated screentime

--- client/src/components/ParentsBackend.js
+++ client/src/components/ParentsBackend.js
@@ -23,25 +23,52 @@ class ParentsBackend extends React.Component {
 
   render() {
     // const children = this.state.children;
-    const children = Object.values(this.state.children).map((child, i) => {
-      return child[i];
+    const children = Object.values(this.state.children).map(child => {
+      const { _id, username, sessionTimes, profileImgUrl } = child;
+      return (
+        <div key={_id}>
+          <div>
+            <img src={profileImgUrl} width="100px" alt="profile image" />
+            <p>
+              <strong>Name:</strong> {username}
+            </p>
+            {sessionTimes.length > 0 && (
+              <p>
+                Screentime:{" "}
+                {sessionTimes.reduce((acc, val) => {
+                  return acc + val;
+                })}
+              </p>
+            )}
+          </div>
+        </div>
+      );
     });
     console.log(this.state.children.username);
 
     return (
       <div>
         <h1>Hello</h1>
-        {/* 
+        <div>{children}</div>
         {this.state.children.length === 0 && (
           <h2>
             No children available. Create <Link to="/childsignup">here</Link>
           </h2>
         )}
-         */}
-        {/* <p>{children}</p> */}
       </div>
     );
   }
 }
 
 export default ParentsBackend;
+
+// parent: [ 5debb1e9c6f1e7751f24aa8b ],
+//   gameStatus: [],
+//   sessionTimes: [ 2.446, 8.801 ],
+//   _id: 5debb1fdc6f1e7751f24aa8c,
+//   username: 'kid1',
+//   password: '$2b$10$IF5c875qziPmYbf3aJ7Un.W5NjHyou.FeDiHjD.kF6XKq0l/iVJse',
+//   birthDate: 0001-01-01T00:00:00.000Z,
+//   profileImgUrl: '/static/media/dragon.4a6b00d5.png',
+//   type: 'child',
+//   __v: 0
