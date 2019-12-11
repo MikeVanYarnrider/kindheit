460588d7b7a69298451336d1b37d8e52a7aa6b80
Michael Garnreiter <mgarnreiter@aol.com>
2019-12-11 13:41:27 +0100

    added screentime tracking to energyflow

--- client/src/components/Games/EnergyFlow/EnergyFlow.js
+++ client/src/components/Games/EnergyFlow/EnergyFlow.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 import AnimationEnergyGame from "../../lottieAnimations/energyGame/AnimationEnergyGame";
 import "../../../assets/stylesheet/_variables.scss";
+import axios from "axios";
 
 class EnergyFlow extends Component {
+  state = {
+    isFinished: false,
+    gameTime: 0
+  };
+
+  postGameTime = () => {
+    const { gameId } = this.props.match.params;
+    const gameEndTime = new Date();
+    const gameTime = (gameEndTime - this.state.gameStartTime) / 1000;
+    axios
+      .post("/child/play/device/energyflow", {
+        gameTime: gameTime,
+        user: this.props.user,
+        game: gameId
+      })
+      .then(response => {
+        console.log(response);
+        this.props.getRestrictionTime(response.data.restricted);
+      })
+      .catch(err => console.log(err));
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      gameStartTime: new Date()
+    });
+    window.addEventListener("beforeunload", this.postGameTime);
+  };
+
+  componentWillUnmount = () => {
+    this.postGameTime();
+    window.removeEventListener("beforeunload", this.postGameTime);
+  };
+
   render() {
     return (
       <div
