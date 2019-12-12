f6d95cc984c35699595421dc65bf189bde36a4b3
Pascal Hanke <pascal.hanke@yahoo.de>
2019-12-12 15:16:03 +0100

    dynamic children passwortImages

--- client/src/components/Signup_Children/ChildrenPassword.js
+++ client/src/components/Signup_Children/ChildrenPassword.js
@@ -19,7 +19,11 @@ const passWordImageArr = [
 class ChildrenPassword extends Component {
   state = {
     passwordImages: this.props.passwordImages,
-    changeImage: this.props.changeImage
+    changeImage: this.props.changeImage,
+    buttonState1: false,
+    buttonState2: false,
+    buttonState3: false,
+    buttonState4: false
   };
 
   signupImageSelect = event => {
@@ -31,7 +35,11 @@ class ChildrenPassword extends Component {
       imageState[imageToBeChanged] = url;
       this.setState({
         passwordImages: this.state.passwordImages,
-        changeImage: 0
+        changeImage: 0,
+        buttonState1: false,
+        buttonState2: false,
+        buttonState3: false,
+        buttonState4: false
       });
     } else if (imageState.length < 4) {
       this.state.passwordImages.push(url);
@@ -39,7 +47,11 @@ class ChildrenPassword extends Component {
     let selectNum = imageState.map(el => el.match(/media[/](\d)/)[1]);
     this.setState({
       passwordImages: this.state.passwordImages,
-      changeImage: 0
+      changeImage: 0,
+      buttonState1: false,
+      buttonState2: false,
+      buttonState3: false,
+      buttonState4: false
     });
 
     this.props.setPasswordImage(this.state.passwordImages);
@@ -48,13 +60,18 @@ class ChildrenPassword extends Component {
 
   signupImageChange = event => {
     let changeImageNum = event.currentTarget.id;
-    this.setState({ changeImage: changeImageNum });
+    console.log(changeImageNum);
+    this.setState({
+      changeImage: changeImageNum,
+      [`buttonState${changeImageNum}`]: true
+    });
     this.props.setChangePassword(changeImageNum);
   };
 
   render() {
     let imageState = this.state.passwordImages;
     let passwordImageSize = "50px";
+    console.log(this.state.buttonState1);
     return (
       <div
         className="passwordWrapper"
@@ -66,7 +83,7 @@ class ChildrenPassword extends Component {
           alignItems: "center"
         }}
       >
-        <section>
+        <section className="passwortChosing">
           {passWordImageArr.map((image, index) => {
             return (
               <button key={index} type="button">
@@ -83,6 +100,7 @@ class ChildrenPassword extends Component {
         </section>
 
         <section
+          className="selectedImages"
           style={{
             width: "500px",
             display: "flex",
@@ -91,21 +109,41 @@ class ChildrenPassword extends Component {
         >
           {[1, 2, 3, 4].map(passwordIndex => {
             return (
-              <button
-                key={passwordIndex}
-                id={passwordIndex}
-                type="button"
-                className="selected-btn"
-                onClick={this.signupImageChange}
-                style={{backgroundColor: "black"}}
-              >
-                <div
-                  className="selectedField"
-                  style={{
-                    backgroundImage: `url(${imageState[passwordIndex - 1]})`
-                  }}
-                ></div>
-              </button>
+              <div key={passwordIndex}>
+                {this.state[`buttonState${passwordIndex}`] ? (
+                  <button
+                    key={passwordIndex}
+                    id={passwordIndex}
+                    type="button"
+                    className="selected-btn active"
+                    onClick={this.signupImageChange}
+                    style={{ backgroundColor: "black" }}
+                  >
+                    <div
+                      className="selectedField active"
+                      style={{
+                        backgroundImage: `url(${imageState[passwordIndex - 1]})`
+                      }}
+                    ></div>
+                  </button>
+                ) : (
+                  <button
+                    key={passwordIndex}
+                    id={passwordIndex}
+                    type="button"
+                    className="selected-btn"
+                    onClick={this.signupImageChange}
+                    style={{ backgroundColor: "black" }}
+                  >
+                    <div
+                      className="selectedField"
+                      style={{
+                        backgroundImage: `url(${imageState[passwordIndex - 1]})`
+                      }}
+                    ></div>
+                  </button>
+                )}
+              </div>
             );
           })}
         </section>
